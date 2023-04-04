5d5e3cb7b2b1dd98720bb4ac20e4992683eab1a4
lys010204 <129822388+lys010204@users.noreply.github.com>
2023-04-04 13:50:51 +0900

    Add files via upload

---
+++ chap02/src/chap02/SecondProgram.java
@@ -0,0 +1,12 @@
+package chap02;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		System.out.println("second");
+		System.out.println("program");
+		System.out.println("second"+"program");
+
+	}
+
+}
